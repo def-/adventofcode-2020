288e0478ad9c8fc88e62aa9592a256e64583a458
def <dennis@felsin9.de>
2020-12-05 23:30:53 +0100

    Improve 5

--- day05.nim
+++ day05.nim
@@ -1,16 +1,16 @@
 #!/usr/bin/env -S nim -r c
-import algorithm, sequtils
-var results: set[int16]
+import algorithm
+var s: array[128*8, bool]
 for l in stdin.lines:
   var i = 0
-  proc m(a, key: auto): int =
-    result = if i == key.len: 0 elif key[i] in "FL": 1 else: -1
+  proc m(s, k: auto): int =
+    result = if i == k.len: 0 elif k[i] in "FL": 1 else: -1
     inc i
-  let r = binarySearch(newSeq[int](128), l[0..^4], m)
-  i = 0
-  let c = binarySearch(newSeq[int](8), l[^3..^1], m)
-  results.incl(int16(r * 8 + c))
-echo max(toSeq(results)) # Part 1
-for i in 0'i16..128*8:
-  if i-1 in results and i notin results and i+1 in results:
+  s[binarySearch(s, l, m)] = true
+for i in countdown(s.high, s.low):
+  if s[i]:
+    echo i # Part 1
+    break
+for i in s.low+1 .. s.high:
+  if s[i-1] and not s[i] and s[i+1]:
     echo i # Part 2
